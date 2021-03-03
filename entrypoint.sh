f6d575dd9bb7de75ff3534ef66642d1417f42fcb
cameron-yee <yee.camero23@gmail.com>
2021-03-03 14:40:25 -0700

    fix: add logging to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,8 @@ while getopts "n:c:o:t:" opt; do
 done
 shift $((OPTIND -1))
 
+echo "${config}"
+echo "${tag}"
 
 if [ -f "${config}/config.yml" ] && [ -f "${config}/CHANGELOG.tpl.md" ]; then
   echo "::debug ::git-chlog: -c '${config}'"
